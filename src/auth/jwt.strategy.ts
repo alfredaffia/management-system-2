81123fa52f12b4ee4d37001c1ae913d2555aa410
alfredaffia <alfredaffia478@gmail.com>
2025-04-29 15:45:55 +0100

    ../

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { ConfigService } from '@nestjs/config';
-import { User } from 'src/user/entities/user.entity';
+import { User } from '../user/entities/user.entity';
 
 
 @Injectable()
